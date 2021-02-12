a3be8d2baa9a53f9b159204f54fc31684a3c59fd
Pablo Pereira Moure <ppereiramoure@danielcastelao.com>
2021-02-12 09:10:04 +0100

    Initial commit

---
+++ src/MisMetodos.java
@@ -0,0 +1,15 @@
+/*
+ * Contiene metodos utiles para usar en los proyectos
+ */
+
+
+public class MisMetodos {
+    /*
+     *  Metodo que monta un saludo
+     *  @param mensaje saludo que quieres mostrar
+     */
+
+    public static saludo(String mensaje){
+      return "Saludo "+ mensaje;
+    }
+}
